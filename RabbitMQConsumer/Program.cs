b55ed7cf73326a12005a7524b19470f77a83a506
Cem ALTINAY <cealtinay@borusan.com>
2022-02-26 14:21:19 +0300

    first commit

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("#####  Beşiktaş Skorbord  ######");
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (IConnection connection = factory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "Besiktas",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Goal? goal = JsonConvert.DeserializeObject<Goal>(message);
+        Console.WriteLine($"GOL ATAN {goal?.PlayerName}-{goal?.AssistName}");
+        Console.WriteLine("Gücüne Güç Katmaya geldik formanda ter olmaya geldik...");
+        Console.WriteLine("........................................................");
+    };
+
+
+    channel.BasicConsume(queue: "Besiktas", autoAck: true, consumer: consumer);
+
+
+    Console.ReadLine();
+}
+public record Goal(string PlayerName, string AssistName, DateTime GoalDate);
---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("BEŞİKTAŞ gol atıyor.........");
+
+STARTPOINT:
+
+Console.Write("Asist Yapan Oyuncu: ");
+var asssitPlayer = Console.ReadLine();
+
+Console.Write("Gol Atan Oyuncu: ");
+var goalPlayer = Console.ReadLine();
+
+var goal1 = new Goal(goalPlayer??"Bilinmiyor", asssitPlayer??"Bilinmiyor", DateTime.UtcNow);
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (IConnection connection = factory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "Besiktas",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    string message = JsonConvert.SerializeObject(goal1);
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "",
+                         routingKey: "Besiktas",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine($"Gole katkı sağlayanlar: {goal1.PlayerName}-{goal1.AssistName}");
+}
+
+Console.WriteLine("Top Kaleye Gönderildi...");
+Console.WriteLine("--------------------------------------------------------------------");
+goto STARTPOINT;
+
+
+
+public record Goal(string PlayerName, string AssistName, DateTime GoalDate);
